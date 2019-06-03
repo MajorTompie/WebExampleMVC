b642ddd6862f3937db08b14f547aa856b703b4a9
MajorTompie <tomverschoor95@gmail.com>
2019-06-03 13:54:00 +0200

    Add project files.

---
+++ WebExampleMVC/Data/WebExampleMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebExampleMVC.Models
+{
+    public class WebExampleMVCContext : DbContext
+    {
+        public WebExampleMVCContext (DbContextOptions<WebExampleMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebExampleMVC.Models.Person> Person { get; set; }
+    }
+}
---
+++ WebExampleMVC/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebExampleMVC.Models
+{
+    public class Person
+    {
+        public int personID { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime birthdate { get; set; }
+    }
+}
